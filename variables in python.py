a5fecd7c7587a4a6a6eb2eab10831e48f7c3327b
parth-programming04 <parthofficial0404@gmail.com>
2026-06-14 10:14:37 +0530

    complete variable file

--- variables in python.py
+++ variables in python.py
@@ -3,7 +3,7 @@
 
 #Strings
 
-taste = "sweet"
+taste = "sweet" # you need inverted comma for strings unlike other data types
 food = "sugar"
 
 eat = taste + " " + food
@@ -19,3 +19,16 @@ rate += 1
 print("I give it rating of: " + str(rate)) # str() is used to convert a variable type. This is know as type casting.
 # We cannot print variable of two diffrent data type. Thus we need to use type casting.
 print(type(rate))
+
+# Float
+
+weight = 23.5
+
+print("I need: " + str(weight) + " grams of it")
+print(type(weight))
+
+#boolean
+
+truth = True
+print("Do I love my sugar:"+ str(truth))
+print(type(truth))
