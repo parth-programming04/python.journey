f1f61a8bba2b4ec933b615ebbd194d75604bb279
parth-programming04 <parthofficial0404@gmail.com>
2026-06-14 10:07:10 +0530

    variable file 1

--- variables in python.py
+++ variables in python.py
@@ -8,5 +8,14 @@ food = "sugar"
 
 eat = taste + " " + food
 
-print( "I love" + eat)
+print( "I love " + eat)
+print(type(eat)) # type() function is used to find data type of a variable.
 
+# Integer
+
+rate = 8
+rate += 1
+
+print("I give it rating of: " + str(rate)) # str() is used to convert a variable type. This is know as type casting.
+# We cannot print variable of two diffrent data type. Thus we need to use type casting.
+print(type(rate))
