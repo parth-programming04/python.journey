1accd4d8ddff5e21cd25c797e19b95113e90a392
parth-programming04 <parthofficial0404@gmail.com>
2026-06-14 09:54:34 +0530

    added missing main file

---
+++ variables in python.py
@@ -0,0 +1,12 @@
+#Diffrent variable present in Python
+#Python doesnt require you to declare variable type.
+
+#Strings
+
+taste = "sweet"
+food = "sugar"
+
+eat = taste + " " + food
+
+print( "I love" + eat)
+
